41783d0c67665c4c244629268dbdb731c51c3a6a
Torui Natsuya <113415824+AmandaMahardika@users.noreply.github.com>
2023-11-13 12:08:54 +0700

    Add files via upload

---
+++ src/main/java/com/example/datateman/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.datateman
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import com.example.datateman.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    val menuTeks = arrayOf("Teman", "Github", "Profile")
+    val menuIcon = arrayOf(R.drawable.house, R.drawable.plus, R.drawable.profile)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val adapter = ViewPagerAdapter(this)
+        binding.viewPager.setAdapter(adapter);
+        TabLayoutMediator(binding.tabLayout, binding.viewPager, { tab, position ->
+                tab.text = menuTeks[position]
+                tab.icon = ResourcesCompat.getDrawable(resources, menuIcon[position], null)
+            }).attach()
+    }
+}
